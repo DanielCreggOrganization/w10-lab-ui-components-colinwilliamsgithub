fa78f574abbc132710e5405858342448f27bfdb1
Colin Williams <123738487+colinwilliamsgithub@users.noreply.github.com>
2024-12-02 15:26:04 +0000

    Added interactive lists

--- ui-components-demo/src/app/tab1/tab1.page.ts
+++ ui-components-demo/src/app/tab1/tab1.page.ts
@@ -7,7 +7,7 @@ import { ExploreContainerComponent } from '../explore-container/explore-containe
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent, IonButton, IonCard, IonCardHeader, IonCardSubtitle, IonCardContent, IonCardTitle ],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent, IonButton, IonCard, IonCardHeader, IonCardSubtitle, IonCardContent, IonCardTitle],
 })
 export class Tab1Page {
   constructor() {}
--- ui-components-demo/src/app/tab2/tab2.page.ts
+++ ui-components-demo/src/app/tab2/tab2.page.ts
@@ -1,16 +1,20 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonItemOption, IonIcon, IonItemOptions, IonLabel, IonItem, IonItemSliding, IonBadge, IonList } from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { addIcons } from 'ionicons';
+import { trash, star, share } from 'ionicons/icons';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent]
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent, IonItemOption, IonIcon, IonItemOptions, IonLabel, IonItem, IonItemSliding, IonBadge, IonList]
 })
 export class Tab2Page {
 
-  constructor() {}
+  constructor() {
+    addIcons({ trash, star, share });
+  }
 
 }
